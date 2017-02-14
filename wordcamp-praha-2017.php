8dd002294d12d8e9f9e44d321ebc9f0d58e21545
david-binda <david.binovec@gmail.com>
2017-02-14 20:35:39 +0000

    Initial plugin file with header - see https://developer.wordpress.org/plugins/the-basics/header-requirements/

---
+++ wordcamp-praha-2017.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WordCamp Praha 2017 Plugin
+Plugin URI:  https://https://github.com/david-binda/wordcamp-praha-2017
+Description: Example plugin with PHPUnit tests
+Version:     0.1
+Author:      David Binovec
+Author URI:  https://https://github.com/david-binda
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wcpraha2017
+Domain Path: /languages
+*/
